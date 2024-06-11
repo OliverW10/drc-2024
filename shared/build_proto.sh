b883fec0550d4b9e48311ff7295596273d03a037
OliverW10 <oliver.warrick2@gmail.com>
2024-06-11 21:41:16 +1000

    move scripts

--- shared/build_proto.sh
+++ shared/build_proto.sh
@@ -3,4 +3,5 @@ mkdir -p ../planner/messages/
 cp messages/*.proto ../planner/messages
 mkdir -p ../client/messages/
 cp messages/*.proto ../client/messages
-
+mkdir -p ../scripts/messages/
+protoc -I=messages/ --python_out="../scripts/messages/" messages/*
